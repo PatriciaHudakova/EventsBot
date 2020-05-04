79656bc1e4dc013932ac97acaa832c8beb9dc295
PatriciaHudakova <phudakova7@gmail.com>
2020-05-04 18:43:53 +0100

    added emojis to all buttons

--- src/functionality.go
+++ src/functionality.go
@@ -7,12 +7,12 @@ import (
 // Initial choice of buttons upon triggered /start command
 func btnStartingChoices() *tbot.InlineKeyboardMarkup {
 	btnCreate := tbot.InlineKeyboardButton{
-		Text:         "Create Event",
+		Text: "Create Event 	\U00002712",
 		CallbackData: "/new",
 	}
 
 	btnHelp := tbot.InlineKeyboardButton{
-		Text:         "See what I can do!",
+		Text: "See what I can do!	\U00002728",
 		CallbackData: "/help",
 	}
 	return &tbot.InlineKeyboardMarkup{
@@ -25,22 +25,22 @@ func btnStartingChoices() *tbot.InlineKeyboardMarkup {
 // Button options for event change in /edit command
 func btnOptionsChoices() *tbot.InlineKeyboardMarkup {
 	btnChangeName := tbot.InlineKeyboardButton{
-		Text:         "Rename",
+		Text:         "Rename \U00002712",
 		CallbackData: "/newName",
 	}
 
 	btnChangeDate := tbot.InlineKeyboardButton{
-		Text:         "Date",
+		Text: "Date 	\U0001F4C5",
 		CallbackData: "/newDate",
 	}
 
 	btnChangeTime := tbot.InlineKeyboardButton{
-		Text:         "Time",
+		Text: "Time 	\U0001F553",
 		CallbackData: "/newTime",
 	}
 
 	btnDelete := tbot.InlineKeyboardButton{
-		Text:         "Delete",
+		Text:         "Delete \U00002757",
 		CallbackData: "/delete",
 	}
 	return &tbot.InlineKeyboardMarkup{
